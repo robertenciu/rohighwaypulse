69c4403601c84612b6170e03853b4ee28d4749f2
robertenciu <robert_enciu@yahoo.com>
2025-06-24 19:19:57 +0300

    Initial commit

---
+++ src/main/java/com/enciu/rohighwaypulse/RohighwaypulseApplication.java
@@ -0,0 +1,13 @@
+package com.enciu.rohighwaypulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RohighwaypulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RohighwaypulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enciu/rohighwaypulse/RohighwaypulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enciu.rohighwaypulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RohighwaypulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
